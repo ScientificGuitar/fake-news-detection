6ad3d7cf55de6cf50842d9536f72ab68d01b7d1e
ScientificGuitar <jancoventer.lol@gmail.com>
2022-08-02 00:40:17 +0200

    lastrefuge scraping script

--- data_gathering/scrape_scripts.py
+++ data_gathering/scrape_scripts.py
@@ -40,10 +40,48 @@ def scrape_americasfreedomfighters(pages=10):
     articles_df = pd.DataFrame(data=articles_list, columns=['source', 'title', 'content'])
     articles_df.to_csv('data/americasfreedomfighters.csv', index=False)
 
+def scrape_lastrefuge(pages=10):
+    articles_list = []
+
+    for page in range(pages):
+        try:
+            print(f'Scraping page: {page}')
+            # Search page
+            url = f'https://theconservativetreehouse.com/page/{page}/'
+            search_page = requests.get(url)
+
+            soup = BeautifulSoup(search_page.content, 'html.parser')
+            articles = soup.find_all('a', class_='more-link')
+            list_articles = [article['href'] for article in articles]
+        except Exception as e:
+            print(e)
+            print(url)
+            print(f'Page {page}\n')
+        for article_url in list_articles:
+            try:
+                article_page = requests.get(article_url)
+
+                article_soup = BeautifulSoup(article_page.content, 'html.parser')
+                title = article_soup.find('article').find('h1').text.strip()
+                title = title.replace(',', '')
+                content_raw = article_soup.find('article').find('div', class_='post-content').find_all('p')
+                content = " ".join([p.text.strip() for p in content_raw])
+                content = content.replace(',', '').replace('\n', '')
+
+                articles_list.append(['lastrefuge', title, content])
+            except Exception as e:
+                print(e)
+                print(article_url)
+    articles_df = pd.DataFrame(data=articles_list, columns=['source', 'title', 'content'])
+    articles_df.to_csv('data/lastrefuge.csv', index=False)
+
+
+
 
 
 
 if __name__ == '__main__':
-    scrape_americasfreedomfighters(1100)
+    #scrape_americasfreedomfighters(1100)
+    scrape_lastrefuge(3954)
 
     
